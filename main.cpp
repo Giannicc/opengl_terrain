708bfa8941f5158b95607ac925cc79d97b20f1bb
Gianni Ciccarelli <ciccarelligc@wooster.edu>
2017-11-29 21:39:26 -0500

    Add working separated include statements

--- main.cpp
+++ main.cpp
@@ -1,15 +1,30 @@
+#ifdef _WIN32
+#include <iostream>
 #include <GLUT/GLUT.H>
+#elif __APPLE__
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#include <GLUT/glut.h>
+#elif __linux__
+/*
+	HEEEEeeeeey Phillip do your includes heeeeeeere
+*/
+#endif
 
+using namespace std;
 void init() {
 
 }
 
 int main(int argc, char **argv) {
+	/*
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH);
 	glutInitWindowSize(500, 500);
 	glutInitWindowPosition(100, 100);
 	glutCreateWindow("OpenGL Terrain");
 	init();
+	*/
+	cout << "Hey some working separate includes" << endl;
 	return 0;
 }
