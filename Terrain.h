928d1fbef142ae664dc40805303e8c53d876cde0
Gianni Ciccarelli <ciccarelligc@wooster.edu>
2017-12-02 14:28:58 -0500

    Add translated code to Terrain files

--- Terrain.h
+++ Terrain.h
@@ -1,9 +1,13 @@
 #include <vector>
 #include <cstdlib>
 #include <time.h>
+#include <math.h>
 using namespace std;
 class RGB {
 public:
+	RGB() {
+		r = g = b = 0;
+	}
 	RGB(double _r, double _g, double _b) {
 		r = _r;
 		g = _g;
@@ -62,10 +66,76 @@ public:
 	FractalTerrain(int lod, double roughness);
 	~FractalTerrain();
 	double **terrain;
+	double rnd();
+	double getAltitude(double i, double j);
+	RGB getColor(double i, double j);
 private:
 	void diamond(int x, int y, int side, double scale);
 	void square(int x, int y, int side, double scale);
 
+	RGB blue = RGB(0.0, 0.0, 1.0),
+		green = RGB(0.0, 1.0, 0.0),
+		white = RGB(1.0, 1.0, 1.0);
 	double roughness, min, max;
 	int divisions;
+};
+/*
+class Triangle {
+public:
+	Triangle(int i0, int j0, int i1, int j1, int i2, int j2) {
+		i[0] = i0;
+		i[1] = i1;
+		i[2] = i2;
+		j[0] = j0;
+		j[1] = j1;
+		j[2] = j2;
+	}
+private:
+	int *i = new int[3];
+	int *j = new int[3];
+	Triple n;
+	RGB *rgb = new RGB[3];
+	Color color;
+};
+*/
+
+class Triple {
+public:
+	Triple() {
+		x = y = z = 0;
+	}
+	Triple(double a, double b, double c) {
+		x = a;
+		y = b;
+		z = c;
+	}
+	Triple add(Triple t) {
+		return Triple(x + t.x, y + t.y, z + t.z);
+	}
+	Triple subtract(Triple t) {
+		return Triple(x - t.x, y - t.y, z - t.z);
+	}
+
+	Triple cross(Triple t) {
+		return Triple(y * t.z - z * t.y, z * t.x - x * t.z,
+			x * t.y - y * t.x);
+	}
+
+	double dot(Triple t) {
+		return x * t.x + y * t.y + z * t.z;
+	}
+
+	double length2() {
+		return dot(*this);
+	}
+
+	Triple normalize() {
+		return scale(1.0 / sqrt(length2()));
+	}
+
+	Triple scale(double scale) {
+		return Triple(x * scale, y * scale, z * scale);
+	}
+private:
+	double x, y, z;
 };
--- main.cpp
+++ main.cpp
@@ -10,7 +10,33 @@
 #include <GL/glu.h>
 #include <GL/glut.h>
 #endif
+#include "Terrain.h"
 using namespace std;
+
+FractalTerrain *terrain;
+
+void terrainGen() {
+	double exaggeration = .7;
+	int lod = 5;
+	int steps = 1 << lod;
+	Triple **map = new Triple*[steps + 1];
+	for (int i = 0; i < steps + 1; i++) {
+		map[i] = new Triple[steps + 1];
+	}
+	RGB **colors = new RGB*[steps + 1];
+	for (int i = 0; i < steps + 1; i++) {
+		colors[i] = new RGB[steps + 1];
+	}
+	terrain = new FractalTerrain(lod, .5);
+	for (int i = 0; i <= steps; ++i) {
+		for (int j = 0; j <= steps; ++j) {
+			double x = 1.0 * i / steps, z = 1.0 * j / steps;
+			double altitude = (*terrain).getAltitude(x, z);
+			map[i][j] = Triple(x, altitude * exaggeration, z);
+			colors[i][j] = (*terrain).getColor(x, z);
+		}
+	}
+}
 
 void display() {
 	glClearColor(0.0, 0.0, 0.0, 1.0);
@@ -23,11 +49,20 @@ void reshape(int w, int h) {
 
 }
 
+void keyboard(unsigned char key, int x, int y) {
+	switch (key) {
+	case 'q':
+		exit(0);
+		break;
+	}
+	return;
+}
+
 void init() {
 	glutDisplayFunc(display);
 	glutIdleFunc(display);
 	glutReshapeFunc(reshape);
-	//Possibly a keyboard function
+	glutKeyboardFunc(keyboard);
 	glEnable(GL_DEPTH_TEST);
 	//Now enable some lighting stuff...
 }
@@ -39,5 +74,6 @@ int main(int argc, char **argv) {
 	glutInitWindowPosition(100, 100);
 	glutCreateWindow("OpenGL Terrain");
 	init();
+	glutMainLoop();
 	return 0;
 }
