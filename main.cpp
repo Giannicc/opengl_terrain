25639deaf2b5609a2649e605a80554377b9bd6bd
Gianni Ciccarelli <ciccarelligc@wooster.edu>
2017-12-07 18:27:37 -0500

    Begin trying to fix shading

--- main.cpp
+++ main.cpp
@@ -48,90 +48,96 @@ void terrainGen() {
     /*
       Shading
     */
-    double ambient = .3;
-    double diffuse = 4.0;
-    Triple **normals = new (nothrow) Triple*[steps + 1];
-    for (int i = 0; i < steps + 1; i++) {
-	normals[i] = new (nothrow) Triple[steps + 1];
-    }
-    Triple sun = Triple(3.6, 3.9, 0.6);
-    double **shade = new (nothrow) double*[steps + 1];
-    for (int i = 0; i < steps + 1; i++) {
-	shade[i] = new (nothrow) double[steps + 1];
-    }
-    for (int i = 0; i <= steps; i++) {
-	for (int j = 0; j <= steps; j++) {
-	    shade[i][j] = 1.0;
-	    Triple vertex = map[i][j];
-	    Triple ray = sun.subtract(vertex);
-	    double distance = steps * sqrt(ray.x * ray.x + ray.z * ray.z);
-	    /* step along ray in horizontal units of grid width */
-	    for (double place = 1.0; place < distance; place += 1.0) {
-		Triple sample = vertex.add(ray.scale(place / distance));
-		double sx = sample.x, sy = sample.y, sz = sample.z;
-		if ((sx < 0.0) || (sx > 1.0) || (sz < 0.0) || (sz > 1.0))
-		    break; /* stepped off terrain */
-		double ground = exaggeration * (*terrain).getAltitude(sx, sz);
-		if (ground >= sy) {
-		    shade[i][j] = 0.0;
-		    break;
+	double ambient = .3;
+	double diffuse = 4.0;
+	vector<vector<Triple>> normals;
+	for (int i = 0; i < steps + 1; i++) {
+		vector<Triple> newVec;
+		normals.push_back(newVec);
+		for (int j = 0; j < steps + 1; j++) {
+			Triple newTrip(0, 0, 0);
+			normals[i].push_back(newTrip);
 		}
-	    }
 	}
-    }
-    /*
-      Creating array of Triangles
-    
-   	for (int i = 0; i < numTriangles; i++) {
+	Triple sun = Triple(3.6, 3.9, 0.6);
+	vector<vector<double>> shade;
+	for (int i = 0; i < steps + 1; i++) {
+		vector<double> newVec;
+		shade.push_back(newVec);
+		for (int j = 0; j < steps + 1; j++) {
+			double dubs = 0;
+			shade[i].push_back(dubs);
+		}
+	}
+	for (int i = 0; i <= steps; i++) {
+		for (int j = 0; j <= steps; j++) {
+			shade[i][j] = 1.0;
+			Triple vertex = map[i][j];
+			Triple ray = sun.subtract(vertex);
+			double distance = steps * sqrt(ray.x * ray.x + ray.z * ray.z);
+			/* step along ray in horizontal units of grid width */
+			for (double place = 1.0; place < distance; place += 1.0) {
+				Triple sample = vertex.add(ray.scale(place / distance));
+				double sx = sample.x, sy = sample.y, sz = sample.z;
+				if ((sx < 0.0) || (sx > 1.0) || (sz < 0.0) || (sz > 1.0))
+					break; /* stepped off terrain */
+				double ground = exaggeration * (*terrain).getAltitude(sx, sz);
+				if (ground >= sy) {
+					shade[i][j] = 0.0;
+					break;
+				}
+			}
+		}
+	}
+
+	for (int i = 0; i < numTriangles; i++) {
 		triangles.push_back(Triangle());
 	}
 
-    int triangle = 0;
+	int triangle = 0;
 	for (int i = 0; i < steps; i++) {
 		for (int j = 0; j < steps; j++) {
 			triangles[triangle++] = Triangle(i, j, i + 1, j, i, j + 1);
 			triangles[triangle++] = Triangle(i + 1, j, i + 1, j + 1, i, j + 1);
 		}
 	}
-	/*
+
 	for (int i = 0; i < numTriangles; i++)
 	for (int j = 0; j < 3; ++j)
-	    normals[i][j] = Triple(0.0, 0.0, 0.0);
-		*/
-	/*
-    // compute triangle normals and vertex averaged normals 
-    for (int i = 0; i < numTriangles; i++) {
-	if (i > 32) i = 0;
-	Triple v0 = map[triangles[i].i[0]][triangles[i].j[0]],
-	    v1 = map[triangles[i].i[1]][triangles[i].j[1]],
-	    v2 = map[triangles[i].i[2]][triangles[i].j[2]];
-	Triple normal = v0.subtract(v1).cross(v2.subtract(v1)).normalize();
-	triangles[i].normal = normal;
-	for (int j = 0; j < 3; ++j) {
-	    normals[triangles[i].i[j]][triangles[i].j[j]] =
-		normals[triangles[i].i[j]][triangles[i].j[j]].add(normal);
-	}
-    }
-    for (int i = 0; i < numTriangles; i++) {
-	RGB avg = RGB(0.0, 0.0, 0.0);
-	for (int j = 0; j < 3; j++) {
-	    int k = triangles[i].i[j], l = triangles[i].j[j];
-	    Triple vertex = map[k][l];
-	    RGB color = colors[k][l];
-	    Triple normal = normals[k][l].normalize();
-	    Triple light = vertex.subtract(sun);
-	    double distance2 = light.length2();
-	    double dot = light.normalize().dot(normal);
-	    double shadow = shade[k][l];
-	    double lighting = ambient + diffuse * ((dot < 0.0) ? -dot : 0.0) /
-		distance2 * shadow;
-	    color = color.scale(lighting);
-	    //triangles[i].color[j] = color;
-	    avg = avg.add(color);
-	}
-	triangles[i].color = RGB(avg.scale(1.0 / 3.0));
-    }
-	*/
+		normals[i][j] = Triple(0.0, 0.0, 0.0);
+
+		// compute triangle normals and vertex averaged normals
+		for (int i = 0; i < numTriangles; i++) {
+		if (i > 32) i = 0;
+		Triple v0 = map[triangles[i].i[0]][triangles[i].j[0]],
+			v1 = map[triangles[i].i[1]][triangles[i].j[1]],
+			v2 = map[triangles[i].i[2]][triangles[i].j[2]];
+		Triple normal = v0.subtract(v1).cross(v2.subtract(v1)).normalize();
+		triangles[i].normal = normal;
+		for (int j = 0; j < 3; ++j) {
+			normals[triangles[i].i[j]][triangles[i].j[j]] =
+			normals[triangles[i].i[j]][triangles[i].j[j]].add(normal);
+		}
+		}
+		for (int i = 0; i < numTriangles; i++) {
+		RGB avg = RGB(0.0, 0.0, 0.0);
+		for (int j = 0; j < 3; j++) {
+			int k = triangles[i].i[j], l = triangles[i].j[j];
+			Triple vertex = map[k][l];
+			RGB color = colors[k][l];
+			Triple normal = normals[k][l].normalize();
+			Triple light = vertex.subtract(sun);
+			double distance2 = light.length2();
+			double dot = light.normalize().dot(normal);
+			double shadow = shade[k][l];
+			double lighting = ambient + diffuse * ((dot < 0.0) ? -dot : 0.0) /
+			distance2 * shadow;
+			color = color.scale(lighting);
+			//triangles[i].color[j] = color;
+			avg = avg.add(color);
+		}
+		triangles[i].color = RGB(avg.scale(1.0 / 3.0));
+		}
 }
 
 void drawTerrain() {
